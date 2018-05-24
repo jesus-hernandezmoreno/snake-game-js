c5a82122ddd3db37c63ce9d450e858182c86579e
Web Wizard <jesus.hernandezmoreno@outlook.com>
2018-05-24 18:57:31 -0500

    ADD():Include the main js file

---
+++ main.js
@@ -0,0 +1,33 @@
+//Game Logic
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+
+//Create the unit
+const box = 32;
+
+//Load Images
+const ground = new Image();
+ground.src = "img/ground.png"
+
+const foodImg = new Image();
+foodImg.src = "img/foodImg";
+
+//Create the sanke
+
+let snake[];
+
+snake[0] = {
+  x : 9 * box,
+  y : 10 * box
+}
+
+//Create the foodImg
+
+let food = {
+  x: Math.floor(Math.random()*17+1) * box,
+  y: Math.floor(Math.random()*15+3) * box
+}
+
+//Create the score
+
+let score = 0;
