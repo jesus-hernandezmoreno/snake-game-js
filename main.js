adb17532f1b319ea20fff4f7324ba1c0abffdd9c
Web Wizard <jesus.hernandezmoreno@outlook.com>
2018-05-24 19:29:44 -0500

    ADD():Validate if snakes eats food

--- main.js
+++ main.js
@@ -81,8 +81,19 @@ function draw(){
     if( d == "RIGHT") snakeX += box;
     if( d == "DOWN") snakeY += box;
 
-    // remove the tail
-    snake.pop();
+    // if the snake eats the food
+      if(snakeX == food.x && snakeY == food.y){
+          score++;
+          eat.play();
+          food = {
+              x : Math.floor(Math.random()*17+1) * box,
+              y : Math.floor(Math.random()*15+3) * box
+          }
+          // we don't remove the tail
+      }else{
+          // remove the tail
+          snake.pop();
+      }
 
     // add new Head
 
